6164a7de601b042d99afab77fd2a4f5b5efff806
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2024-02-13 10:22:14 +0530

    conversion of data types

---
+++ JS_tutorials/03_conversion_operation.js
@@ -0,0 +1,33 @@
+/*
+let score = 33
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = Number(score) // converting score string to Number
+console.log(typeof value) // getting the type of data stored in variable - value
+console.log(value)
+
+// conversion of different data types
+
+// "33" -> Number
+// "SHUV" -> NaN (Not a Number)
+// true -> 1 , false -> 0
+
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// according to following isLoggedIn values, 
+// corresponding Boolean conversion values of them
+
+// 1 -> true , 0 -> false
+// "" -> false
+// "SHUV" -> true
+
+let num = 33
+let strNum = String(num) // Converting number to String
+console.log(strNum)
+console.log(typeof strNum)
+
