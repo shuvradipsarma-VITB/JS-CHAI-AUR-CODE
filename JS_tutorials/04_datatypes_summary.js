2044b5fc9d4ceaf66481ff04b4c674a477c494e3
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2024-02-22 23:48:49 +0530

    changes

---
+++ JS_tutorials/04_datatypes_summary.js
@@ -0,0 +1,16 @@
+
+// Primitive data types - their values are copied when used later in code after initialization step.
+// Reference/ Non primitive data types - their values are obtained by the object reference
+
+// primitive  -  String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100 
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
---
+++ JS_tutorials/o4_comparison.js
@@ -0,0 +1,15 @@
+
+console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null>=0)
+
+// strict check - checks the comparison including their data types
+console.log("2" === 2) // false since string compared with integer
